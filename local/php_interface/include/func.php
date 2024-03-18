9e460a23d5357e31f8d9e7a893799834534b0068
Vladimir <jasonstadzam@gmail.com>
2024-03-19 01:42:39 +0300

    Задание 1: Добавление функций для файла init.php

---
+++ local/php_interface/include/func.php
@@ -0,0 +1,17 @@
+<?php
+
+use Bitrix\Highloadblock\HighloadBlockTable;
+use Bitrix\Main\Loader;
+
+function getHlId($table): int|false {
+    Loader::IncludeModule('highloadblock');
+
+    $rs = HighloadBlockTable::getList([
+        'filter' => ['=TABLE_NAME' => $table]
+    ])->fetch();
+
+    return $rs['ID'] ?? false;
+}
+
+
+
---
+++ local/php_interface/init.php
@@ -0,0 +1,3 @@
+<?php
+
+include 'include/func.php';
