013cb0c613b6ca492eb2b8853fe96f78a59664d4
Vladimir <jasonstadzam@gmail.com>
2024-03-19 18:52:25 +0300

    Задание 3: Создал страницу /add-product/

---
+++ add-product/.section.php
@@ -0,0 +1,5 @@
+<?php
+
+$sSectionName = 'Добавить товар';
+$arDirProperties = [];
+
---
+++ add-product/index.php
@@ -0,0 +1,11 @@
+<?php
+
+require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php');
+$APPLICATION->SetTitle("Добавить товар");
+?>
+<?php include $_SERVER['DOCUMENT_ROOT'] . SITE_TEMPLATE_PATH . '/include/pages/add-product/index.php' ?>
+<?php
+
+require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php');
+?>
+
